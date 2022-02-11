8af4c3e30b76e92481043bd8a45bc755a3c68e1b
Emil Korniluk <emil.korniluk@gmail.com>
2022-02-11 23:49:57 +0100

    minor changes to the SetupDriver class

--- src/main/java/Setup/SetupDriver.java
+++ src/main/java/Setup/SetupDriver.java
@@ -14,54 +14,52 @@ import java.util.Properties;
 
 public class SetupDriver {
 
-    static WebDriver driver = null;
+    private static WebDriver driver;
 
-
+    private static Properties loadPropertyFile() throws IOException {
+        Properties properties = new Properties();
+        FileInputStream propData = new FileInputStream("src/main/resources/data.properties");
+        properties.load(propData);
+        return properties;
+    }
 
     private static String getBrowserNameFromProperty() throws IOException {
-        Properties properties = new Properties();
-        FileInputStream propData = new FileInputStream("src/main/resources/data.properties");
-        properties.load(propData);
-        return properties.getProperty("browser");
+        return loadPropertyFile().getProperty("browser");
     }
-
 
     private static String getUrlFromProperty() throws IOException {
-        Properties properties = new Properties();
-        FileInputStream propData = new FileInputStream("src/main/resources/data.properties");
-        properties.load(propData);
-        return properties.getProperty("Url");
+        return loadPropertyFile().getProperty("Url");
     }
-
-
 
     public static WebDriver loadDriver() throws IOException {
         String browserName = getBrowserNameFromProperty();
+        String url = getUrlFromProperty();
+
         switch (browserName) {
             case "chrome" -> {
                 WebDriverManager.chromedriver().setup();
-                SetupDriver.driver = new ChromeDriver();
+                driver = new ChromeDriver();
             }
             case "internet explorer" -> {
                 WebDriverManager.iedriver().setup();
-                SetupDriver.driver = new InternetExplorerDriver();
+                driver = new InternetExplorerDriver();
             }
             case "firefox" -> {
                 WebDriverManager.firefoxdriver().setup();
-                SetupDriver.driver = new FirefoxDriver();
+                driver = new FirefoxDriver();
             }
-            case "Edge" -> {
+            case "edge" -> {
                 WebDriverManager.edgedriver().setup();
-                SetupDriver.driver = new EdgeDriver();
+                driver = new EdgeDriver();
             }
-            default -> System.out.println("This browser " + browserName + "not suported. Please change browser.");
+            default -> System.out.println("This browser " + browserName + " not supported. Please change browser.");
         }
-        SetupDriver.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        SetupDriver.driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
-        SetupDriver.driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 
-        SetupDriver.driver.get(getUrlFromProperty());
+        driver.get(url);
 
-        return SetupDriver.driver;
+        return driver;
     }
 }
